59158193e205a3dfadf7462f43a087c07d558f36
Raül Box <rbv.bove@gmail.com>
2026-05-01 21:28:35 +0200

    delete: fake_wiringpi

--- Entrega2/Lab 6 - Comunicació UART PIC-Raspberry/raspberry/fake_wiringpi.c
+++
@@ -1,6 +0,0 @@
-// stubs vacíos para satisfacer el linker
-int wiringPiSetupGpio(void) { return 0; }
-void pinMode(int a, int b) {}
-void digitalWrite(int a, int b) {}
-int digitalRead(int a) { return 0; }
-void delay(unsigned int ms) {}

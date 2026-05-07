86481557959376e39555f1a7b0466e669a296074
solinaJoan <joan.solina@estudiantat.upc.edu>
2026-05-07 18:56:35 +0200

    update: touchpad functionality with light sensor
    Refactor touchpad code to include light sensor reading and modify send function to include light status.

--- Entrega4/pic/touchpad.c
+++ Entrega4/pic/touchpad.c
@@ -4,31 +4,42 @@
 #define FCY 4000000
 #include <libpic30.h>
 
-// Variable global compartida con la ISR
+
+#define LIGHT_HIGH 550
+#define LIGHT_LOW  460
+
 volatile char rx = '1';
-volatile int state = 0; // 0 don't send; 1 send
+volatile int send_status = 0; // 0 don't send; 1 send
+volatile uint8_t light_status = 0; // 0 bright mode; 1 dark mode
 
 
 /************   ANALOG READ **********************/
 
+void configure2readLDR() {
+    // AN12 -> RB12: input analógico
+    TRISBbits.TRISB12 = 1;
+    AD1PCFGbits.PCFG12 = 0;  // analogico
+}
+
+
 void configure2readX() {
     // X+: AN3 -> RB1: output digital, valor 1
     TRISBbits.TRISB1 = 0;
-    AD1PCFGbits.PCFG3 = 1;   // digital  (era ANSBbits.ANSB1 = 0)
+    AD1PCFGbits.PCFG3 = 1;   // digital
     LATBbits.LATB1 = 1;
 
     // X-: AN2 -> RB0: output digital, valor 0
     TRISBbits.TRISB0 = 0;
-    AD1PCFGbits.PCFG2 = 1;   // digital  (era ANSBbits.ANSB0 = 0)
+    AD1PCFGbits.PCFG2 = 1;   // digital
     LATBbits.LATB0 = 0;
 
     // Y+: AN11 -> RB13: input digital
     TRISBbits.TRISB13 = 1;
-    AD1PCFGbits.PCFG11 = 1;  // digital  (era ANSBbits.ANSB13 = 0)
+    AD1PCFGbits.PCFG11 = 1;  // digital
 
     // Y-: AN10 -> RB14: input anal?ico
     TRISBbits.TRISB14 = 1;
-    AD1PCFGbits.PCFG10 = 0;  // anal?ico (era ANSBbits.ANSB14 = 1)
+    AD1PCFGbits.PCFG10 = 0;  // analogico
 }
 
 void configure2readY() {
@@ -65,9 +76,9 @@ void configure_ADC() {
     //   ADRC = 1  -> clock RC interno (independiente de Fcy)
     //   SAMC<4:0> = 10000 -> 16 TAD de muestreo autom?ico
     //AD1CON3 = 0x801F;
-      AD1CON3bits.ADRC =  0; // Clock derived from system clock
-  AD1CON3bits.SAMC = 31; // Sample time = 31 Tad
-  AD1CON3bits.ADCS =  0; // Tad = 2*Tcy
+    AD1CON3bits.ADRC =  0; // Clock derived from system clock
+    AD1CON3bits.SAMC = 31; // Sample time = 31 Tad
+    AD1CON3bits.ADCS =  0; // Tad = 2*Tcy
 
     // AD1CSSL: sin scan
     AD1CSSL = 0x0000;
@@ -89,25 +100,16 @@ static uint16_t readADC(uint8_t channel) {
     return ADC1BUF0;            // resultado 10 bits (0..1023)
 }
 
+uint16_t readLDR() {
+    return readADC(12);
+}
+
 uint16_t readCoordX() {
     return readADC(10);         // CH0SA = 10 -> AN10
 }
 
 uint16_t readCoordY() {
     return readADC(2);          // CH0SA = 2  -> AN2
-}
-
-int is_touched() {
-    // X+ (RB1) -> HIGH, X- (RB0) -> LOW
-    TRISBbits.TRISB1 = 0; LATBbits.LATB1 = 1;
-    TRISBbits.TRISB0 = 0; LATBbits.LATB0 = 0;
-
-    // Y+ (RB13) -> input digital
-    TRISBbits.TRISB13 = 1;
-    AD1PCFGbits.PCFG11 = 1;
-
-    __delay_us(50);
-    return PORTBbits.RB13; // 1 = tocat, 0 = no tocat
 }
 
 /***********   UART   *************************/
@@ -120,30 +122,21 @@ void configure_UART() {
                   "MOV.b w2, [w1]     \n"
                   "MOV.b w3, [w1]     \n"
                   "BSET OSCCON, #6");
-
-
-  /***********************************************/
+    
   // Baud rate (9600)
-  /***********************************************/
   U1MODEbits.BRGH = 1;
   U1BRG = 103;
 
-  /***********************************************/
   // 8N1
-  /***********************************************/
   U1MODEbits.RXINV = 0;
   U1MODEbits.PDSEL = 0;
   U1MODEbits.STSEL = 0;
-
-  /***********************************************/
+  
   // Enable UART
-  /***********************************************/
   U1MODEbits.UARTEN = 1;
   U1STAbits.UTXEN = 1;
-
-  /***********************************************/
+  
   // RX Interrupt
-  /***********************************************/
   IEC0bits.U1RXIE = 1;
   IFS0bits.U1RXIF = 0;
   IPC2bits.U1RXIP = 4;
@@ -164,11 +157,13 @@ void send_UART () {
    U1TXREG = rx;
 }
 
-void send_coords(uint16_t x, uint16_t y) {
+void send_info(uint16_t x, uint16_t y, uint8_t light) {
+    while (U1STAbits.UTXBF); U1TXREG = 0xAA;
     while (U1STAbits.UTXBF); U1TXREG = (uint8_t)(x & 0xFF);        // X low
     while (U1STAbits.UTXBF); U1TXREG = (uint8_t)((x >> 8) & 0xFF); // X high
     while (U1STAbits.UTXBF); U1TXREG = (uint8_t)(y & 0xFF);        // Y low
     while (U1STAbits.UTXBF); U1TXREG = (uint8_t)((y >> 8) & 0xFF); // Y high
+    while (U1STAbits.UTXBF); U1TXREG = light;    // light
     while (U1STAbits.UTXBF); U1TXREG = 0xAA;
 }
 
@@ -188,27 +183,32 @@ int main(void) {
   configure_pins();
   configure_UART();
   configure_ADC();
-  uint16_t x, y;
+  uint16_t x, y, light;
   
   // Activar interrupciones 
   __builtin_enable_interrupts();
   
   while(1) {
-      state = PORTAbits.RA0;
-      LATAbits.LATA1 = state;
+      send_status = PORTAbits.RA0;
+      LATAbits.LATA1 = send_status;
+      
+      if (send_status) {
+        configure2readX();
+        __delay_ms(5);
+        x = readCoordX();
+        configure2readY();
+        uint16_t y = readCoordY();
+        
+        configure2readLDR();
+        light = readLDR();
+        
+        // dark_mode && light over threshold -> bright mode
+        // if (light_status && light < LIGHT_HIGH) light_status = 0;
+        // bright mode && light below threshold -> dark mode
+        // else if (!light_status && light > LIGHT_LOW) light_status = 1;
 
-      if (state) {
-          //if (is_touched()) {
-            configure2readX();
-            __delay_ms(10);
-            x = readCoordX();
-            configure2readY();
-            uint16_t y = readCoordY();
-
-            //send_UART();
-            send_coords(x, y);
-            __delay_ms(5);
-          //}
+        send_info(x, y, (uint8_t)(light >> 2));
+        __delay_ms(5);
       }
   }
 
